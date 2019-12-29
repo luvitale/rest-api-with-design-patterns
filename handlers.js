940253c1c3f80dcfe61b87fefd2ab2f06da4e87f
Luciano Nahuel Vitale <lunavi97@gmail.com>
2019-12-29 14:27:14 -0300

    Returned user data

--- handlers.js
+++ handlers.js
@@ -1,6 +1,6 @@
 module.exports = ({ axios }) => ({
     get: async (req, res) => {
         const { data } = await axios.get('/users')
-        res.send(data)
+        return res.send(data)
     }
 })
