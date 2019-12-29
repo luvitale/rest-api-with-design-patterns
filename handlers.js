875fa111a20eb567d9d70951004310b4765a933f
Luciano Nahuel Vitale <lunavi97@gmail.com>
2019-12-29 14:41:51 -0300

    Done post, put and delete methods

--- handlers.js
+++ handlers.js
@@ -2,5 +2,17 @@ module.exports = ({ axios }) => ({
     get: async (req, res) => {
         const { data } = await axios.get('/users')
         return res.send(data)
-    }
+    },
+    post: async (req, res) => {
+        const { data } = await axios.post('/users', req.body)
+        return res.send(data)
+    },
+    put: async (req, res) => {
+        const { data } = await axios.put('/users/${req.params.id}', req.body)
+        return res.send(data)
+    },
+    delete: async (req, res) => {
+        await axios.delete('/users/${req.params.id}')
+        return res.sendStatus(204)
+    },
 })
--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const app = express()
 const port = 3000
 
 app.get('/', handlers(services).get)
+app.post('/', handlers(services).post)
+app.put('/:id', handlers(services).put)
+app.delete('/:id', handlers(services).delete)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
